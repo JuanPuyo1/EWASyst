45d961ae6cd4199de62987d5721e7108e6ca307b
Esteban Puyo <esteban.cubi1@gmail.com>
2025-03-19 23:14:20 -0500

    adddin the first configuration for login into the app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.views import View
+from .forms import LoginForm
 # Create your views here.
+
+class LoginModifiedView(View):
+    form_class = LoginForm
+    template_name = 'login.html'
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                return redirect('login')
+        return render(request, self.template_name, {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
