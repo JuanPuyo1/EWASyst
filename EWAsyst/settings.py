665f52a8c7829aa4363864a7b32b637d2bdeec53
Esteban Puyo <esteban.cubi1@gmail.com>
2025-03-19 22:57:07 -0500

    adding the picocss framework for styling, and registering the apps into settings

--- EWAsyst/settings.py
+++ EWAsyst/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
+    'clients',
+    'inventories',
+    'invoices',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
