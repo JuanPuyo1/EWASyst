b8898b289c5737f2e222f5fcd08ce064e488a50c
Esteban Puyo <esteban.cubi1@gmail.com>
2025-03-19 23:41:24 -0500

    adding the dashboard app

--- EWAsyst/urls.py
+++ EWAsyst/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('', include('dashboard.urls')),
     path('clients/', include('clients.urls')),
     path('inventories/', include('inventories.urls')),
     path('invoices/', include('invoices.urls')),
