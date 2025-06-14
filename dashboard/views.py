35b28f9664c95dc001e62994f83d7cedb32b7515
Esteban Puyo <esteban.cubi1@gmail.com>
2025-06-14 02:46:08 -0500

    adding the first implementation into the dashboard panel

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from invoices.models import InvoiceForItem, InvoiceForMaintenance, InvoiceForStone, InvoiceForCustomized
 # Create your views here.
 
 class DashboardView(LoginRequiredMixin, View):
     def get(self, request):
-        return render(request, 'dashboard/dashboard.html')
+        # Count all types of invoices
+        total_invoices = (
+            InvoiceForItem.objects.count() +
+            InvoiceForMaintenance.objects.count() +
+            InvoiceForStone.objects.count() +
+            InvoiceForCustomized.objects.count()
+        )
+
+        context = {
+            'title': 'Dashboard',
+            'user': request.user,
+            'total_invoices': total_invoices,
+        }
+
+        return render(request, 'dashboard/dashboard.html', context)
+    
+
+
+   
+
