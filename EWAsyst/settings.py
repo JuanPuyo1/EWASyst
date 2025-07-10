74ff213d3d42da4a10f802b56d21b9345d3ce5ff
Esteban Puyo <esteban.cubi1@gmail.com>
2025-07-10 00:31:49 -0500

    adding the compose.yml and testing

--- EWAsyst/settings.py
+++ EWAsyst/settings.py
@@ -87,11 +87,17 @@ WSGI_APPLICATION = 'EWAsyst.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.{}'.format(
+             os.environ.get('DATABASE_ENGINE', 'sqlite3')
+         ),
+         'NAME': os.getenv('DATABASE_NAME', 'ewa'),
+         'USER': os.getenv('DATABASE_USERNAME', 'postgres'),
+         'PASSWORD': os.getenv('DATABASE_PASSWORD', '123456'),
+         'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+         'PORT': os.getenv('DATABASE_PORT', 5432),
+     }
+ }
 
 
 # Password validation
