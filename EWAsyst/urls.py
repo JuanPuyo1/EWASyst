f4bdcce29543a13d00456e802502d82393fa0bcf
Esteban Puyo <esteban.cubi1@gmail.com>
2025-07-16 23:07:42 -0500

    editing the dockerfile

--- EWAsyst/urls.py
+++ EWAsyst/urls.py
@@ -29,3 +29,4 @@ urlpatterns = [
     path('invoices/', include('invoices.urls')),
     path('quotes/', include('quotes.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
